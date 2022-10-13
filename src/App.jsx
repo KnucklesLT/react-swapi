7628042f0828c4698229efbbdfd2ca27c4c77d7a
Luigi T <luigitejada@yahoo.com>
2022-10-12 21:00:58 -0400

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import StarshipsList from './pages/StarshipsList/StarshipsList';
+import Header from './components/Header';
 
 function App() {
   return (
   <>
+    <Header />
     <Routes>
       <Route path='/' element={<StarshipsList />}/>
     </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return ( 
+    <header className="App-header">
+      <Link to='/'>STAR WARS STARSHIPS</Link>
+    </header>
+  );
+}
+
+export default Header;
